5563d4f6b1ba90f48a29c4eb3007daab27b12ae4
Malindu <gimsaramalindu2002@gmail.com>
2026-07-14 14:42:22 +0530

    feat: Add return tea type and amount fields to factory log management

--- backend/factory/models/FactoryLog.js
+++ backend/factory/models/FactoryLog.js
@@ -14,15 +14,19 @@ const factoryLogSchema = new mongoose.Schema({
   
   // --- DISPATCH DETAILS ---
   dispatch: { type: Number, default: 0 },
-  invoiceNo: { type: String, default: '' },          // අලුතින් එකතු කළ Field එක
-  dispatchTeaType: { type: String, default: '' },    // අලුතින් එකතු කළ Field එක
+  invoiceNo: { type: String, default: '' },          
+  dispatchTeaType: { type: String, default: '' },    
   
   // --- LOCAL SALE DETAILS ---
   localSaleAndGratis: { type: Number, default: 0 },
-  localSaleTeaType: { type: String, default: '' },   // අලුතින් එකතු කළ Field එක
+  localSaleTeaType: { type: String, default: '' },   
   
   totalOut: { type: Number, default: 0 },
+
+  // --- RETURN DETAILS ---
   returnAmount: { type: Number, default: 0 },
+  returnTeaType: { type: String, default: '' },      // අලුතින් එකතු කළ Field එක (Return Tea Type)
+  
   bfBalance: { type: Number, default: 0 },
   factoryBalance: { type: Number, default: 0 },
   
